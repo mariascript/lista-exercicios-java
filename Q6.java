dfe0096ba7636a74c607c34b797985207e6a6491
Maria Pereira <mariaaaa229@gmail.com>
2026-04-18 20:18:42 -0300

    refatoração do código para transformar em função

--- Q6.java
+++ Q6.java
@@ -1,6 +1,5 @@
 import java.util.Locale;
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Q6 {
     public static void main(String[] args) {
@@ -8,22 +7,35 @@ public class Q6 {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-
         double[] tempos = new double[n];
 
         for (int i = 0; i < tempos.length; i++) {
             tempos[i] = sc.nextDouble();
         }
-        Arrays.sort(tempos);
+
+        processar(tempos);
+
+        sc.close();
+    }
+
+    public static void processar(double[] tempos) {
+
+        for (int i = 0; i < tempos.length - 1; i++) {
+            for (int j = 0; j < tempos.length - 1 - i; j++) {
+                if (tempos[j] > tempos[j + 1]) {
+                    double temp = tempos[j];
+                    tempos[j] = tempos[j + 1];
+                    tempos[j + 1] = temp;
+                }
+            }
+        }
 
         double menor = tempos[0];
         double maior = tempos[tempos.length - 1];
 
-        System.out.printf("Melhor tempo: %.1f\n", menor);
-        System.out.printf("Pior tempo: %.1f\n", maior);
-        System.out.println("Tres melhores tempos:");
+        System.out.printf("melhor tempo: %.1f\n", menor);
+        System.out.printf("pior tempo: %.1f\n", maior);
+        System.out.println("tres melhores tempos:");
         System.out.printf("%.1f %.1f %.1f\n", tempos[0], tempos[1], tempos[2]);
-
-        sc.close();
     }
 }
