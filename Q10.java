dcf6b0dd5d274ecfe9ed3cb32e34ce9b2f77d8bb
Maria Pereira <mariaaaa229@gmail.com>
2026-04-19 08:55:35 -0300

    refatoração do código para transformar em função

--- Q10.java
+++ Q10.java
@@ -4,58 +4,70 @@ import java.util.Scanner;
 public class Q10 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            int l = sc.nextInt();
+            int c = sc.nextInt();
 
-        int l = sc.nextInt();
-        int c = sc.nextInt();
+            int[][] matriz = new int[l][c];
 
-        int[][] mat = new int[l][c];
+            for (int i = 0; i < l; i++) {
+                for (int j = 0; j < c; j++) {
+                    matriz[i][j] = sc.nextInt();
+                }
+            }
 
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                mat[i][j] = sc.nextInt();
+            int[] somaLinhas = somaLinhas(matriz);
+            int[] somaColunas = somaColunas(matriz);
+
+            for (int i = 0; i < somaLinhas.length; i++) {
+                System.out.println("soma da linha " + i + ": " + somaLinhas[i]);
+            }
+
+            for (int j = 0; j < somaColunas.length; j++) {
+                System.out.println("soma da coluna " + j + ": " + somaColunas[j]);
+            }
+
+            System.out.println("linha com maior soma: " + maiorIndice(somaLinhas));
+            System.out.println("coluna com maior soma: " + maiorIndice(somaColunas));
+        }
+    }
+
+    public static int[] somaLinhas(int[][] matriz) {
+        int[] somas = new int[matriz.length];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                somas[i] += matriz[i][j];
             }
         }
 
-        int maiorSomaLinha = Integer.MIN_VALUE;
-        int indiceL = 0;
+        return somas;
+    }
 
-        for (int i = 0; i < mat.length; i++) {
-            int soma = 0;
+    public static int[] somaColunas(int[][] matriz) {
+        int[] somas = new int[matriz[0].length];
 
-            for (int j = 0; j < mat[i].length; j++) {
-                soma += mat[i][j];
-            }
-
-            System.out.println("Soma da linha " + i + ": " + soma);
-
-            if (soma > maiorSomaLinha) {
-                maiorSomaLinha = soma;
-                indiceL = i;
+        for (int j = 0; j < matriz[0].length; j++) {
+            for (int i = 0; i < matriz.length; i++) {
+                somas[j] += matriz[i][j];
             }
         }
 
-        int maiorSomaColuna = Integer.MIN_VALUE;
-        int indiceC = 0;
+        return somas;
+    }
+    
+    public static int maiorIndice(int[] vetor) {
+    int maior = vetor[0];
+    int indice = 0;
 
-        for (int j = 0; j < mat[0].length; j++) {
-            int soma = 0;
-
-            for (int i = 0; i < mat.length; i++) {
-                soma += mat[i][j];
-            }
-
-            System.out.println("Soma da coluna " + j + ": " + soma);
-
-            if (soma > maiorSomaColuna) {
-                maiorSomaColuna = soma;
-                indiceC = j;
-            }
+    for (int i = 1; i < vetor.length; i++) {
+        if (vetor[i] > maior) {
+            maior = vetor[i];
+            indice = i;
         }
+    }
 
-        System.out.println("Linha com maior soma: " + indiceL);
-        System.out.println("Coluna com maior soma: " + indiceC);
-
-        sc.close();
+    return indice;
+        
     }
 }
