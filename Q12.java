ce600206b3730a618ccade05b0d1884200723b07
Maria Pereira <mariaaaa229@gmail.com>
2026-04-19 09:23:11 -0300

    refatoração do código para transformar em função
    Refactor matrix handling by adding helper methods for reading and reorganizing the matrix.

--- Q12.java
+++ Q12.java
@@ -4,45 +4,61 @@ import java.util.Scanner;
 public class Q12 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
 
-        int l = sc.nextInt();
-        int c = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int l = sc.nextInt();
+            int c = sc.nextInt();
 
+            int[][] mat = lerMatriz(sc, l, c);
+
+            int r = sc.nextInt();
+            int s = sc.nextInt();
+
+            if (l * c != r * s) {
+                System.out.println("reorganizacao impossivel");
+                return;
+            }
+
+            int[][] novaMatriz = reorganizarMatriz(mat, r, s);
+
+            System.out.println("Matriz reorganizada: ");
+
+            for (int i = 0; i < novaMatriz.length; i++) {
+                for (int j = 0; j < novaMatriz[i].length; j++) {
+                    System.out.print(novaMatriz[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static int[][] lerMatriz(Scanner sc, int l, int c) {
         int[][] mat = new int[l][c];
 
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
                 mat[i][j] = sc.nextInt();
             }
         }
 
-        int r = sc.nextInt();
-        int s = sc.nextInt();
+        return mat;
+    }
 
-        if (l * c != r * s) {
-            System.out.println("Reorganizacao impossivel");
-            sc.close();
-            return;
-        }
-
-        System.out.println("Matriz reorganizada:");
-
+    public static int[][] reorganizarMatriz(int[][] mat, int r, int s) {
+        int[][] novaMatriz = new int[r][s];
         int k = 0;
+        int c = mat[0].length;
 
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < s; j++) {
-
                 int linhaOriginal = k / c;
                 int colunaOriginal = k % c;
 
-                System.out.print(mat[linhaOriginal][colunaOriginal] + " ");
-
+                novaMatriz[i][j] = mat[linhaOriginal][colunaOriginal];
                 k++;
             }
-            System.out.println();
         }
 
-        sc.close();
+        return novaMatriz;
     }
 }
