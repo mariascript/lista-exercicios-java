27d7a79a1393798987b6f5a0c8cd89ccb666c1ef
Maria Pereira <mariaaaa229@gmail.com>
2026-04-19 10:36:10 -0300

    refatoração do código para transformar em função

--- Q17.java
+++ Q17.java
@@ -4,10 +4,25 @@ import java.util.Scanner;
 public class Q17 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
 
-        int l = sc.nextInt();
-        int c = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int l = sc.nextInt();
+            int c = sc.nextInt();
+
+            int[][] matriz = lerMatriz(sc, l, c);
+
+            int[] resultadosLinha = maiorSomaLinha(matriz);
+            int[] resultadoColuna = maiorSomaColuna(matriz);
+
+            System.out.println("maior soma contigua em linha: " + resultadosLinha[0]);
+            System.out.println("linha da maior soma: " + resultadoLinha[1]);
+            System.out.println();
+            System.out.println("maior soma contigua em coluna: " + resultadoColuna[0]);
+            System.out.println("coluna da maior soma: " + resultadoColuna[1]);
+        }
+    }
+
+    public static int[][] lerMatriz(Scanner sc, int l, int c) {
         int[][] matriz = new int[l][c];
 
         for (int i = 0; i < l; i++) {
@@ -16,32 +31,40 @@ public class Q17 {
             }
         }
 
-        int maiorLinha = matriz[0][0];
-        int indiceSomaL = 0;
+        return matriz;
+    }
 
-        for (int i = 0; i < l; i++) {
+    public static int[] maiorSomaLinha(int[][] matriz) {
+        int maiorLinha = matriz[0][0];
+        int indiceLinha = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
             int somaAtual = matriz[i][0];
             int melhor = matriz[i][0];
 
-            for (int j = 1; j < c; j++) {
+            for (int j = 1; j < matriz[i].length; j++) {
                 somaAtual = Math.max(matriz[i][j], somaAtual + matriz[i][j]);
                 melhor = Math.max(melhor, somaAtual);
             }
 
             if (melhor > maiorLinha) {
                 maiorLinha = melhor;
-                indiceSomaL = i;
+                indiceLinha = i;
             }
         }
 
+        return new int[]{maiorLinha, indiceLinha};
+    }
+
+    public static int[] maiorSomaColuna(int[][] matriz) {
         int maiorColuna = matriz[0][0];
         int indiceColuna = 0;
 
-        for (int j = 0; j < c; j++) {
+        for (int j = 0; j < matriz[0].length; j++) {
             int somaAtual = matriz[0][j];
             int melhor = matriz[0][j];
 
-            for (int i = 1; i < l; i++) {
+            for (int i = 1; i < matriz.length; i++) {
                 somaAtual = Math.max(matriz[i][j], somaAtual + matriz[i][j]);
                 melhor = Math.max(melhor, somaAtual);
             }
@@ -52,12 +75,6 @@ public class Q17 {
             }
         }
 
-        System.out.println("Maior soma contigua em linha: " + maiorLinha);
-        System.out.println("Linha da maior soma: " + indiceSomaL);
-        System.out.println("");
-        System.out.println("Maior soma contigua em coluna: " + maiorColuna);
-        System.out.println("Coluna da maior soma: " + indiceColuna);
-
-        sc.close();
+        return new int[]{maiorColuna, indiceColuna};
     }
 }
