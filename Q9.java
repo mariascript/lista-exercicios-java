68c915ba84b61b5b56ac5c0a251c16e8014d2933
Maria Pereira <mariaaaa229@gmail.com>
2026-04-19 08:33:35 -0300

    refatoração do código para transformar em função

--- Q9.java
+++ Q9.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class Q9 {
+
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
@@ -9,38 +10,47 @@ public class Q9 {
         int n = sc.nextInt();
         int[][] matriz = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
                 matriz[i][j] = sc.nextInt();
             }
         }
 
-        int somaPrincipal = 0;
-        int somaSecundaria = 0;
-        int somaTotalSemRepetir = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (i == j) {
-                    somaPrincipal += matriz[i][j];
-                    somaTotalSemRepetir += matriz[i][j];
-                }
-
-                if (i + j == n - 1) {
-                    somaSecundaria += matriz[i][j];
-
-                    if (i != j) {
-                        somaTotalSemRepetir += matriz[i][j];
-                    }
-                }
-            }
-        }
-
-        System.out.println("soma da diagonal principal: " + somaPrincipal);
-        System.out.println("soma da diagonal secundaria: " + somaSecundaria);
-        System.out.println("soma total das diagonais sem repetir o centro: " + somaTotalSemRepetir);
+        System.out.println("soma da diagonal principal: " + somaPrincipal(matriz));
+        System.out.println("soma da diagonal secundaria: " + somaSecundaria(matriz));
+        System.out.println("soma total das diagonais sem repetir o centro: " + somaTotalSemRepetir(matriz));
 
         sc.close();
+    }
+
+    public static int somaPrincipal(int[][] matriz) {
+        int soma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            soma += matriz[i][i];
+        }
+        return soma;
+    }
+
+    public static int somaSecundaria(int[][] matriz) {
+        int soma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            soma += matriz[i][matriz.length - 1 - i];
+        }
+        return soma;
+    }
+    
+    public static int somaTotalSemRepetir(int[][] matriz) {
+        int soma = 0;
+        
+        for (int i = 0; i < matriz.length; i++) {
+            soma += matriz[i][i];
+            
+            if (i != matriz.length - 1 - i) {
+                soma += matriz[i][matriz.length - 1 - i];
+            }
+        }
+        return soma;
     }
 }
